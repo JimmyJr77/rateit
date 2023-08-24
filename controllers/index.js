40e075cf9f3a1efaa5c5998db2abf1dda71f7aa8
Jacob Medina <jacobmedina1618@gmail.com>
2023-08-24 19:02:35 -0400

    added file structure

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 
-//const apiRoutes = require('./api');
+const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const categoriesRoutes = require('./categories-routes.js');
+const toolsRoutes = require('./tools-routes.js');
 
 router.use('/', homeRoutes);
-//router.use('/api', apiRoutes);
+router.use('/api', apiRoutes);
+router.use('/categories', categoriesRoutes);
+router.use('/tools', toolsRoutes);
 
 module.exports = router;
