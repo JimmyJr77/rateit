c90e827e11e92bbfc0851d12f23d149d6310d6db
Jacob Medina <jacobmedina1618@gmail.com>
2023-08-24 20:44:29 -0400

    uncommented router uses

--- controllers/index.js
+++ controllers/index.js
@@ -5,9 +5,9 @@ const homeRoutes = require('./home-routes.js');
 const categoriesRoutes = require('./categories-routes.js');
 const toolsRoutes = require('./tools-routes.js');
 
-// router.use('/', homeRoutes);
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-// router.use('/categories', categoriesRoutes);
-// router.use('/tools', toolsRoutes);
+router.use('/categories', categoriesRoutes);
+router.use('/tools', toolsRoutes);
 
 module.exports = router;
