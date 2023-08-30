72793598266b9bc3f25bfc3be76e0c2de3e929a2
jimmyjr77 <jimmyjr.obrien@gmail.com>
2023-08-29 21:59:53 -0400

    Created a path to incorporate the stars into the html

--- public/js/ratings.js
+++ public/js/ratings.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+    const catRoutes = require('../controllers/categories-routes.js');
+
     // TOOL BOX DROP DOWN TOGGLE
 
     function toggleDropdown(element) {
@@ -31,65 +33,51 @@ $(document).ready(function() {
     }
 
 
-    // RENDER STARS FOR OVERALL RATING
 
-    // This is assuming 'data' is the array containing the "tool" data
-    data.forEach(function(tool) {
-        renderStars(tool["reviewCharacteristics.overall_rating"], tool.id);
-    });
+    module.exports = {
+        renderStars: (value) => {
 
-    function renderStars(value, id) {
-        // Your existing code...
-        document.getElementById('overall-rating-stars-' + id).innerHTML = starsHTML;
-    }
+            const maxStars = 5;
+            let fullStars = Math.floor(value);
+            let decimalPart = value - fullStars;
+            
+            let extraStar = 0;  // This will be either 0 or 1
+            let halfStar = 0;  // This will be either 0 or 1
+            
+            if (decimalPart >= 0 && decimalPart <= 0.3) {
+                extraStar = 0;
+            } else if (decimalPart >= 0.4 && decimalPart <= 0.6) {
+                halfStar = 1;
+            } else if (decimalPart >= 0.7 && decimalPart <= 0.9) {
+                extraStar = 1;
+            } 
+            
+            let totalStars = fullStars + extraStar;  // Full star count
+            let emptyStars = maxStars - totalStars - halfStar; // Empty star count
+            
+            let starsHTML = "";
 
-    //var value = 3.5; // change this value to be generated dynamically
+            // Render full stars
+            for(let i = 0; i < fullStars; i++) {
+                starsHTML += `<span class="bi bi-star-fill yellow"></span>`;
+            }
 
-    function renderStars(value) {
-    const maxStars = 5;
-    let fullStars = Math.floor(value);
-    let decimalPart = value - fullStars;
-    
-    let extraStar = 0;  // This will be either 0 or 1
-    let halfStar = 0;  // This will be either 0 or 1
-    
-    if (decimalPart >= 0 && decimalPart <= 0.3) {
-        extraStar = 0;
-    } else if (decimalPart >= 0.4 && decimalPart <= 0.6) {
-        halfStar = 1;
-    } else if (decimalPart >= 0.7 && decimalPart <= 0.9) {
-        extraStar = 1;
-    } 
-    
-    let totalStars = fullStars + extraStar;  // Full star count
-    let emptyStars = maxStars - totalStars - halfStar; // Empty star count
-    
-    let starsHTML = "";
+            // Render extra full-star if applicable
+            for(let i = 0; i < extraStar; i++) {
+                starsHTML += `<span class="bi bi-star-fill yellow"></span>`;
+            }
 
-    // Render full stars
-    for(let i = 0; i < fullStars; i++) {
-        starsHTML += `<span class="bi bi-star-fill yellow"></span>`;
-    }
+            // Render half-star if applicable
+            for(let i = 0; i < halfStar; i++) {
+                starsHTML += `<span class="bi bi-star-half yellow"></span>`;
+            }
 
-    // Render extra full-star if applicable
-    for(let i = 0; i < extraStar; i++) {
-        starsHTML += `<span class="bi bi-star-fill yellow"></span>`;
-    }
+            // Render empty stars
+            for(let i = 0; i < emptyStars; i++) {
+                starsHTML += `<span class="bi bi-star yellow"></span>`;
+            }
 
-    // Render half-star if applicable
-    for(let i = 0; i < halfStar; i++) {
-        starsHTML += `<span class="bi bi-star-half yellow"></span>`;
-    }
-
-    // Render empty stars
-    for(let i = 0; i < emptyStars; i++) {
-        starsHTML += `<span class="bi bi-star yellow"></span>`;
-    }
-
-    document.getElementById('overall-rating-stars').innerHTML = starsHTML;
-    }
-
-    // Call function to render stars
-    renderStars(value);
-
+            return `<span id="overall-rating-stars" class="star-rating">${starsHTML}</span>`;
+        },
+    };
 });
